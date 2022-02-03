981f811d7d5d69247aa526c72a5674b9f86149fe
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-02-03 17:03:15 +0100

    added multiple states to allow user input

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-import ExpenseDate from "../Expenses/ExpenseDate";
 import './ExpenseForm.css'
 const ExpenseForm = () => {
-return (
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  }
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value)
+  }
+
+  return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
             <label>Title</label>
-            <input type="text"/>
+            <input type="text" onChange={titleChangeHandler}/>
         </div>
         <div className="new-expense__controls">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01"/>
+            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__controls">
             <label>Date</label>
-            <input type="date" min="2020-01-01" max="2022-02-01"/>
+            <input type="date" min="2020-01-01" max="2022-02-01" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
