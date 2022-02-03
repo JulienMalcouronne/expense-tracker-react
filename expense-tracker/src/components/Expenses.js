2c64aaf7890fb2b1289a422b7b99ccfa4190b167
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-02-03 14:22:48 +0100

    added children props for more reusable code

--- expense-tracker/src/components/Expenses.js
+++ expense-tracker/src/components/Expenses.js
@@ -1,8 +1,9 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
+import Card from "./Card";
 
 function Expenses (props) {
-  return <div className="expenses">
+  return <Card className="expenses">
     <ExpenseItem
       title={props.items[0].title}
       amount={props.items[0].amount}
@@ -18,7 +19,7 @@ function Expenses (props) {
       amount={props.items[2].amount}
       date={props.items[2].date}>
     </ExpenseItem>
-  </div>
+  </Card>
 }
 
 export default Expenses;
