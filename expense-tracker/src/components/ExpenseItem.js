f2e6f4e7ec14e0a483cddbe79eedf0e99a7a6853
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-02-03 11:19:39 +0100

    splitted component date expense and addes styling to it

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">€250.72</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
